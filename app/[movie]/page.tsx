3d078f15cb1790f952128823fb6b2bad2076e330
Nouelle <ncprs10@gmail.com>
2023-07-20 13:45:21 +0800

    TEST

--- app/[movie]/page.tsx
+++ app/[movie]/page.tsx
@@ -1,8 +1,9 @@
 export default async function MovieDetail({ params }: any) {
   const { movie } = params;
   const imagePath = "https://image.tmdb.org/t/p/original";
+  const key = process.env.API_KEY;
   const data = await fetch(
-    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movie}?api_key=${key}`
   );
   const res = await data.json();
   console.log(res);
