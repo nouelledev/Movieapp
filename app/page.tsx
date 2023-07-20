a34abf1e4dc7a02281a10e980df58978f999a88b
Nouelle <ncprs10@gmail.com>
2023-07-20 15:36:03 +0800

    Minor updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,34 @@
-import Image from "next/image";
-import { FC } from "react";
 import MovieInfo from "./components/Movie";
 export default async function Home() {
+  const API_URL = "https://api.themoviedb.org/3";
+  const SEARCH_MOVIE = "/search/keyword";
+  const POPULAR_MOVIE = "/movie/popular";
   const data = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+    `${API_URL}/${POPULAR_MOVIE}?api_key=${process.env.API_KEY}`
   );
   const res = await data.json();
   console.log(res);
   console.log(res);
+  const searchMovies = () => {};
   return (
-    <div className=" text-white w-fit h-full">
-      {res.results?.map((movie: any) => (
-        <MovieInfo
-          key={movie.id}
-          id={movie.id}
-          title={movie.title}
-          release_date={movie.release_date}
-          poster_path={movie.poster_path}
-          overview={movie.overview}
-        />
-      ))}
-    </div>
+    <>
+      <h1 className="text-center text-white text-6xl">Nouelle Flixer</h1>
+      <form>
+        <input type="text"></input>
+        <button type="submit">Search movies</button>
+      </form>
+      <div className=" text-white w-fit h-full">
+        {res.results?.map((movie: any) => (
+          <MovieInfo
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            release_date={movie.release_date}
+            poster_path={movie.poster_path}
+            overview={movie.overview}
+          />
+        ))}
+      </div>
+    </>
   );
 }
