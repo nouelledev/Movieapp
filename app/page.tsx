a202344d0b5a7925527429e884276be42e400847
Nouelle <ncprs10@gmail.com>
2023-07-21 16:37:52 +0800

    Fixeddd

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,18 @@ import { useEffect, useState } from "react";
 import MovieInfo from "./components/Movie";
 import getPopular from "./api/popular/route";
 import getTopRated from "./api/toprated/route";
+import Search from "./components/Search";
 export default async function Home() {
   const popularMovieData = await getPopular();
-  const topRatedMovieData = await getTopRated();
+  const topRatedMovie = await getTopRated();
   return (
     <>
       <div className="grid grid-cols-5 text-white w-[80%] h-full mx-auto gap-11 mb-[10%]">
         <h1 className="text-white col-span-3 text-4xl">Trending Movies</h1>
         <div className="col-start-5 col-span-1">
-          <div className="flex flex-row">
-            <form className="rounded-md pt-5">
-              <input
-                type="text"
-                className="md:max-w-[210px] text-black"
-              ></input>
-              <button className="text-black bg-white">Search</button>
-            </form>
-          </div>
+          <Search />
         </div>
-        {popularMovieData.results?.map((movie: any) => (
+        {popularMovieData?.results?.map((movie: any) => (
           <MovieInfo
             key={movie.id}
             id={movie.id}
@@ -36,7 +29,7 @@ export default async function Home() {
       </div>
       <div className="grid grid-cols-5 text-white w-[80%] h-full mx-auto gap-11">
         <h1 className="text-white col-span-5 text-4xl">Top Rated Movies</h1>
-        {topRatedMovieData.results?.map((movie: any) => (
+        {topRatedMovie?.results?.map((movie: any) => (
           <MovieInfo
             key={movie.id}
             id={movie.id}
